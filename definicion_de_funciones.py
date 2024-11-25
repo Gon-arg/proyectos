5a4514d83ab207b3936defbc090478366ea555ab
Gon-arg <mrgonzalo223@gmail.com>
2024-11-25 18:12:47 -0300

    Add files via upload

---
+++ definicion_de_funciones.py
@@ -0,0 +1,29 @@
+"""def name():#primero definimos el nombre de la variable
+    print("hola")#ingresar que queremos que diga la variable
+name()#mostrar en pantalla la funcion"""
+
+"""def name(nombre):#ingresamos un argumento dentro de name
+    print("hola",nombre)#unimos el argumento nombre
+name("gonzalo")#definimos el argumento como gonzalo"""
+
+"""def media(num1,num2,num3):
+    resultado=num1+num2+num3
+    resultado=resultado/3
+    return resultado
+llamada=media(2,2,4)
+print(round(llamada))"""
+
+"""variable=3#variable global
+def funcion():
+    variable=1#variable local
+    print(variable)
+funcion()
+print(variable)"""
+
+"""valores=[1,2,3,4,5]#asignamos valores
+def num(valores):
+    suma=sum(valores)#los sumamos con variable sum
+    return suma#el resultado 15
+suma=sum(valores)
+print(suma)"""
+
